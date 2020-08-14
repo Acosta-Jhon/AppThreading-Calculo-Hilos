256d98cd9c3f1d0da0275d9f78ee9899b54ebebd
Acosta-Jhon <jhonerick2018@gmail.com>
2020-08-13 21:44:28 -0500

    primer commit

---
+++ AppTreading.Consola/Program.cs
@@ -0,0 +1,59 @@
+﻿using AppTreading.Entidades;
+using System;
+using System.Threading;
+
+namespace AppTreading.Consola
+{
+    class Program
+    {
+        private static bool valor = true; // = false - no ejecuta - Ejecucion de Hilos
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------------------CALCULO FIBONACCI-----------------------------");
+
+
+
+            Estudiante estudiante = new Estudiante();
+            Console.ForegroundColor = ConsoleColor.Green;
+            estudiante.CalcularFibonacci(valor);
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("---------------------HILO e HILOS MULTIPLES-----------------------------");
+            int numeroHilos = 0;
+
+            //Crea multiples hilos
+            for (numeroHilos = 0; numeroHilos < 5; numeroHilos++)
+            {
+                Thread primerHilo = new Thread(PrimerHilo);
+                primerHilo.Start(numeroHilos);
+            }
+            //Primer Hilo
+            int numero = 0;
+            while (valor)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray; // <= Color de Hilo
+                Console.WriteLine("Accion realizada desde el Hilo 1 - numero : {0}", numero);
+                numero++;
+
+                if (numero == 15)
+                    valor = false;
+            }
+
+
+        }
+        // Hilo/multiple
+        static void PrimerHilo(object entrada)
+        {
+            int incrementar = 0;
+            while (valor)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue; // Color de Hilo
+                Console.WriteLine("Accion realizada desde el Hilo {0} con {1}", entrada, incrementar);
+                incrementar++;
+            }
+        }
+
+    }
+}
---
+++ AppTreading.Entidades/Estudiante.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace AppTreading.Entidades
+{
+    public class Estudiante
+    {
+        //Propiedades
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        //Metodos
+        public int CalcularFibonacci(bool valor)
+        {
+
+            int x = 1, y = 0;
+            int resultado = 1;
+            Console.Out.WriteLine($"Calculo Fibonacci : {1}");
+            for (int contador = 0; contador < 25; contador++)
+            {
+
+                resultado = x + y;
+                Console.Out.WriteLine($"Calculo Fibonacci : {resultado}");
+                y = x;
+                x = resultado;
+            }
+            
+            return Convert.ToInt32(resultado);
+        }
+    }
+}
